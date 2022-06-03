42fe2ac4c8a2694a2c53fc3138243764243c2652
akshayiosdeveloper <akshaychaudhary55@gmail.com>
2022-06-03 13:47:16 +0530

    Learn SwiftUI Basics

---
+++ Learn-SwiftUI/Learn-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learn-SwiftUI
+//
+//  Created by Akshay Kumar on 03/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Learn Environment object.")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learn-SwiftUI/Learn-SwiftUI/Learn_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_SwiftUIApp.swift
+//  Learn-SwiftUI
+//
+//  Created by Akshay Kumar on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Learn_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
