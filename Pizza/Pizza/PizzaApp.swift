3669fc63f46b44d6c512d2bf830aea89588e2727
akshayiosdeveloper <akshaychaudhary55@gmail.com>
2022-06-06 12:35:18 +0530

    SwiftUI Project

---
+++ Pizza/Pizza/PizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaApp.swift
+//  Pizza
+//
+//  Created by Akshay Kumar on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct PizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
